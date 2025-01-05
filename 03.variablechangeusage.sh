91ed27748224377c60ad9e3da81c761944e247e4
PranayJack Golle <cpranay63@gmail.com>
2025-01-05 15:09:16 +0530

    using variables concept

---
+++ 03.variablechangeusage.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "$PERSON1: hi iam $PERSON1"
+echo "$PERSON2: hi iam $PERSON2, how are you"
+echo "$PERSON1: iam good, how abt you $PERSON2"
+echo "$PERSON2: iam fine"
