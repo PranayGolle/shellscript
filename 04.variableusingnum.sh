73ee85602479a2dc03cc2704f73dec17ae5b934d
PranayJack Golle <cpranay63@gmail.com>
2025-01-05 15:17:02 +0530

    jus using an arguments while execution

---
+++ 04.variableusingnum.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON=$1
+PERSON=$2
+
+echo "$PERSON1: hi iam $PERSON1"
+echo "$PERSON2: hi iam $PERSON2, how are you"
+echo "$PERSON1: iam good, how abt you $PERSON2"
+echo "$PERSON2: iam fine"
