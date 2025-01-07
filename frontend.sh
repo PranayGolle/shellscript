f0a546fcda3c70c421b0079bf00bcb198bd3c5b4
PranayJack Golle <cpranay63@gmail.com>
2025-01-08 00:31:04 +0530

    updating backend

--- frontend.sh
+++ frontend.sh
@@ -26,5 +26,8 @@ VALIDATE $? "moving to html folder"
 unzip /tmp/frontend &>>LOG_FILE_NAME
 VALIDATE $? "unzip the dowloaded content"
 
+cp /home/ec2-user/expense.conf /etc/nginx/default.d/expense.conf &>>LOG_FILE_NAME
+VALIDATE $? "moving conf file"
+
 systemctl restart nginx &>>LOG_FILE_NAME
 VALIDATE $? "restarting the nginx"
