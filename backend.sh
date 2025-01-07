1e9c04d99e1dfc27ad0517b9a908b41558dfd8c3
PranayJack Golle <cpranay63@gmail.com>
2025-01-07 21:21:49 +0530

    modified backend

--- backend.sh
+++ backend.sh
@@ -1,33 +1,8 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./source.sh
 
-LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1 )
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
-}
-
-echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
-
-if [ $USERID -ne 0 ]
-then
-    echo "ERROR:: You must have sudo access to execute this script"
-    exit 1 #other than 0
-fi
+CHECK_ROOT
 
 dnf module disable nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "disable existing version of node"
@@ -38,14 +13,20 @@ VALIDATE $? "enable node version 20"
 dnf install nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "installing backend"
 
-useradd expense &>>$LOG_FILE_NAME
-VALIDATE $? "adding a user for our project"
+id expense $>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then
+    useradd expense &>>$LOG_FILE_NAME
+    VALIDATE $? "adding expense user"
+else
+    echo -e "expense user already exists.. $Y SKIPPING $N"
+fi
 
-mkdir /app
+mkdir /app  &>>$LOG_FILE_NAME
+VALIDATE $? "creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
 VALIDATE $? "downloading backend zip file"
-rm -p /app
 
 cd /app
 unzip /tmp/backend.zip &>>$LOG_FILE_NAME
@@ -59,8 +40,14 @@ cp /ec2-user/shellscript/backend.service /etc/systemd/system/backend.service
 dnf install mysql -y &>>$LOG_FILE_NAME
 VALIDATE $? "installing sql client"
 
-mysql -h mysql.pranayaws.site -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.pranayaws.site -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "setting up schema and tables"
 
-systemctl daemon-reload
-systemctl restart backend
-systemctl enable backend
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "daemon reload"
+
+systemctl restart backend &>>$LOG_FILE_NAME
+VALIDATE $? "restart backend"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? "enable backend"
