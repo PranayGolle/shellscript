9f98b68a2f716fe12bb3ac2cd086efd066329210
PranayJack Golle <cpranay63@gmail.com>
2025-01-08 00:17:42 +0530

    updating backend

---
+++ frontend.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source ./source.sh
+
+CHECK_ROOT
+mkdir -p $LOGS_FOLDER
+
+dnf install nginx -y &>>LOG_FILE_NAME
+VALIDATE $? "installing nginx"
+
+systemctl enable nginx &>>LOG_FILE_NAME
+VALIDATE $? "enabling nginx"
+
+systemctl start nginx &>>LOG_FILE_NAME
+VALIDATE $? "starting nginx"
+
+rm -rf /usr/share/nginx/html/* &>>LOG_FILE_NAME
+VALIDATE $? "removing older content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>LOG_FILE_NAME
+VALIDATE $? " dowloading latest updates"
+
+cd /usr/share/nginx/html &>>LOG_FILE_NAME
+VALIDATE $? "moving to html folder"
+
+unzip /tmp/frontend. &>>LOG_FILE_NAME
+VALIDATE $? "unzip the dowloaded content"
+
+systemctl restart nginx &>>LOG_FILE_NAME
+VALIDATE $? "restarting the nginx"
