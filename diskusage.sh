74eb84ef26a14c8a50fc3671ca123ede14465394
PranayJack Golle <cpranay63@gmail.com>
2025-01-12 19:55:05 +0530

    source file modified finding older files

--- diskusage.sh
+++ diskusage.sh
@@ -5,6 +5,6 @@ THRESHOLD=5 #generally we take 70 in normal servers
 
 while read -r line
 do
-    USAGE=$(echo $line | awk -F " " '{print $6F}')
+    USAGE=$(echo $line | awk -F " " '{print $6F}'| cut -d "%" -f1)
     echo "Print disk usage: $USAGE"
 done <<< $DISK_USAGE
