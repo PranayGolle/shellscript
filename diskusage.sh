4c76a82c5b809451e4d9060993695af5970cff74
PranayJack Golle <cpranay63@gmail.com>
2025-01-12 19:49:12 +0530

    source file modified finding older files

--- diskusage.sh
+++ diskusage.sh
@@ -3,7 +3,8 @@
 DISK_USAGE=$(df -hT | grep xfs)
 THRESHOLD=5 #generally we take 70 in normal servers
 
-while read -r disk
+while read -r line
 do
-    echo "Print disk usage: $DISK_USAGE"
+    USAGE=$($DISK_USAGE | awk -F " " '{print $6F}')
+    echo "Print disk usage: $USAGE"
 done
